3df563df7b8576d4fe97098ff16322eb1676a8e2
jalbas2000 <jalbas2000@yahoo.com>
2020-03-24 22:23:06 +0400

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,31 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "./Details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { observer } from 'mobx-react-lite'
 import ActivityStore from './../../../app/store/activityStore'
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 const ActivityDashboard: React.FC = () => {
 
-  const activityStore = useContext(ActivityStore)
-  const {editMode, selectedActivity} = activityStore
-  
+  const activityStore = useContext(ActivityStore);
+
+  useEffect(() => {
+    activityStore.loadActivities()
+  }, [activityStore])
+
+  if(activityStore.loadingInitial) return <LoadingComponent content='Loading activities ...' />
+
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && !editMode && (
-          <ActivityDetails />
-        )}
-        {editMode && (
-          <ActivityForm
-            key={(selectedActivity && selectedActivity.id) || 0}
-            activity={selectedActivity!}
-          />
-        )}
+        <h2>Activty filters</h2>
       </Grid.Column>
     </Grid>
   );
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,55 +1,68 @@
-import React, { useState, FormEvent, useContext } from "react";
+import React, { useState, FormEvent, useContext, useEffect } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
-import { v4 as uuid } from 'uuid'
-import ActivityStore from './../../../app/store/activityStore'
+import { v4 as uuid } from "uuid";
+import ActivityStore from "./../../../app/store/activityStore";
 import { observer } from "mobx-react-lite";
+import { RouteComponentProps } from "react-router-dom";
 
-interface IProps {
-  activity: IActivity;
+interface DetailParams {
+  id: string;
 }
 
-const ActivityForm: React.FC<IProps> = ({
-  activity: initialFormState
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    activity: initialFormState,
+    loadActivity,
+    clearActivity
+  } = activityStore;
 
-  const activityStore = useContext(ActivityStore)
-  const {createActivity, editActivity, submitting, cancelFormOpen} = activityStore
+  const [activity, setActivity] = useState<IActivity>({
+    id: "",
+    title: "",
+    description: "",
+    category: "",
+    date: "",
+    city: "",
+    venue: ""
+  });
 
-  const initializeForm = () => {
-    if (initialFormState) {
-      return initialFormState;
-    } else {
-      return {
-        id: "",
-        title: "",
-        description: "",
-        category: "",
-        date: "",
-        city: "",
-        venue: ""
-      };
+  useEffect(() => {
+    if (match.params.id && activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
+      );
     }
-  };
+    return () => {
+      clearActivity()
+    }
+  }, [loadActivity, match.params.id, clearActivity, initialFormState, activity.id.length]);
 
-  const [activity, setActivity] = useState<IActivity>(initializeForm);
-
-  const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = event.currentTarget;
     setActivity({ ...activity, [name]: value });
   };
 
   const handleSubmit = () => {
-      if(activity.id.length === 0) {
-        let newActivity = {
-          ...activity,
-          id:uuid()
-        }
-        createActivity(newActivity)
-      } else {
-        editActivity(activity)
-      }
-  }
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid()
+      };
+      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+    } else {
+      editActivity(activity).then(() => history.push(`/activities/${activity.id}`));;
+    }
+  };
 
   return (
     <Segment clearing>
@@ -92,9 +105,15 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder="Venue"
           value={activity.venue}
         />
-        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
-          onClick={cancelFormOpen}
+          loading={submitting}
+          floated="right"
+          positive
+          type="submit"
+          content="Submit"
+        />
+        <Button
+          onClick={() => history.push('/activities')}
           floated="right"
           type="button"
           content="Cancel"
