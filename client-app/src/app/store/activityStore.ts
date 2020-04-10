f7f9359635f87b33e99c004a863d473476951d22
jalbas2000 <jalbas2000@yahoo.com>
2020-04-10 22:38:37 +0400

    End of section 13

--- client-app/src/app/store/activityStore.ts
+++ client-app/src/app/store/activityStore.ts
@@ -1,13 +1,21 @@
 import { history } from './../../index';
 import { IActivity } from './../models/activity';
-import { observable, action, computed, runInAction, configure } from 'mobx'
-import { createContext, SyntheticEvent } from 'react'
+import { observable, action, computed, runInAction } from 'mobx'
+import { SyntheticEvent } from 'react'
 import agent from '../api/agent';
 import { toast } from 'react-toastify';
+import { RootStore } from './rootStore';
 
-configure({enforceActions: 'always'})
 
-class ActivityStore {
+
+export default class ActivityStore {
+    rootStore: RootStore
+
+    constructor(rootStore: RootStore)
+    {
+        this.rootStore = rootStore;
+    }
+
     @observable activityRegistry = new Map();
     @observable activity: IActivity | null = null
     @observable loadingInitial = false
@@ -143,4 +151,3 @@ class ActivityStore {
     }
 }
 
-export default createContext(new ActivityStore())
